e352c3a4ef6b0f677fbcc6002b40b07c0a23e9bf
Gaston Lucero <gaberrini@hotmail.com>
2020-06-24 22:35:53 -0300

    Add test cases

---
+++ phone_number_interpreter/tests/test_phone_number_validator.py
@@ -0,0 +1,30 @@
+"""Tests for phone_number_validator module"""
+import unittest
+from phone_number_interpreter.phone_number_validator import GreekPhoneNumberValidator
+
+
+class TestNaturalNumbersInterpreter(unittest.TestCase):
+
+    def test_greek_phone_number_validator_true_cases(self):
+        # Data
+        test_data = [('00306970241352', True),
+                     ('00302970241352', True),
+                     ('2970241352', True),
+                     ('6970241352', True)]
+
+        # When/Then
+        for data in test_data:
+            assert data[1] == GreekPhoneNumberValidator().validate(data[0])
+
+    def test_greek_phone_number_validator_false_cases(self):
+        # Data
+        test_data = [('00306870241352', False),
+                     ('00303970241352', False),
+                     ('20303970241352', False),
+                     ('69303970241352', False),
+                     ('0030241352', False),
+                     ('0030691352', False)]
+
+        # When/Then
+        for data in test_data:
+            assert data[1] == GreekPhoneNumberValidator().validate(data[0])
